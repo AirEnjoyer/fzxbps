1765dba4899b0f3a158823229887733a8d779f85
nick <nicholasmilligan2019@outlook.com>
2025-11-06 15:55:12 -0800

    Fixed root detection

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,13 @@
 int main(int argc, char *argv[]) {
   uid_t euid = geteuid();
   if (euid == 0) {
-    std::cerr << "Must be run with root privileges\n";
-    return 1;
-  } else {
     std::string query;
     if (argc == 2) {
       std::string arg1 = argv[1];
       if (arg1 == "-i") {
         query = "xbps-query -Rs '*' | awk '{print $2}' | fzf --prompt=\"Select "
                 "package(s) to install: \" --multi "
-                "--border | xargs -ro sudo xbps-install -Sy";
+                "--border | xargs -ro xbps-install -Sy";
       } else if (arg1 == "-r") {
         query = "xbps-query -m | xargs -n1 xbps-uhelper getpkgname | fzf "
                 "--prompt=\"Select package(s) to remove: \" --multi "
@@ -26,6 +23,9 @@ int main(int argc, char *argv[]) {
       return 1;
     }
     std::system(query.c_str());
+  } else {
+    std::cerr << "Must be run with root privileges\n";
+    return 1;
   }
 
   return 0;
